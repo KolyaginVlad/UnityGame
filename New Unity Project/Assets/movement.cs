d5370ce0c9affd3993ebb7d5ee6fd826563389dc
KolyaginVlad <50174448+KolyaginVlad@users.noreply.github.com>
2020-10-18 18:00:21 +0300

    cube move

---
+++ New Unity Project/Assets/movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Vector3 vector;
+    public float speed;
+    public float rot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        vector.z =  Input.GetAxis("Vertical");
+        vector.x = Input.GetAxis("Horizontal");
+        transform.Translate(vector * speed);
+    }
+}
